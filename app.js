aeb7d15b6864399ce8efb55417abd884bb9df224
dewisnu <wisnuari4658@gmail.com>
2023-12-02 01:15:17 +0800

    enchament: add deploy cloud run step

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 
 // app.get('/', (req, res) => {
@@ -11,5 +12,5 @@ app.use('/',require('./src/routes/index'))
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`Example app listening at http://localhost:${PORT} and this is database url ${DATABASE_URL}`);
 });
