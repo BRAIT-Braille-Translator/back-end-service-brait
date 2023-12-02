e4b947ab73ac43c2831e77a7da9601a75a8df478
Irhan Nugroho <irhan9001@gmail.com>
2023-12-02 11:55:25 +0800

    update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 exports.getHelloWord = async () => {
     try {
-        return 'Hello Ari'
+        return 'Hello Ar'
     } catch (error) {
         console.error(error);
         throw new Error('Internal Server Error');
