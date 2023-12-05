90e362f395f77f0dd704144d5507e33fc7a53e09
Irhan Nugroho <irhan9001@gmail.com>
2023-12-05 18:16:59 +0800

    feat: menambahkan app. js dan .env

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
 
-
-app.use('/',require('./src/routes/index'))
-
+app.use("/", require("./src/routes/index"));
+app.use("/api/users", require("./src/routes/userRoutes"));
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT} and this is database url are ${DATABASE_URL}`);
+  console.log(
+    `Example app listening at http://localhost:${PORT} and this is database url are ${DATABASE_URL}`
+  );
 });
