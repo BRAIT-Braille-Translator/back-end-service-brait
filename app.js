17ab11a223c6c28a83021a54ac7020a8e7676fb1
dewisnu <wisnuari4658@gmail.com>
2023-12-18 00:40:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
+const PATH = process.env.BASE_PATH_BRAIT_MODEL_ML
 
 app.use(bodyParser.json())
 app.use('/',require('./src/routes/index'))
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT} and this is database url are ${DATABASE_URL}`);
+  console.log(`Example app listening at http://localhost:${PORT} and this is database url are ${DATABASE_URL} thi is path ${PATH}`);
 });
