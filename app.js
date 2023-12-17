e9890ffd4c3db12ba21801d280cff11d785acde2
dewisnu <wisnuari4658@gmail.com>
2023-12-18 00:47:54 +0800

    fix: env problem

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
-const PATH = process.env.BASE_PATH_BRAIT_MODEL_ML
 
 app.use(bodyParser.json())
 app.use('/',require('./src/routes/index'))
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT} and this is database url are ${DATABASE_URL} thi is path ${PATH}`);
+  console.log(`Example app listening at http://localhost:${PORT} and this is database url are ${DATABASE_URL}`);
 });
